bd97cc0f03c492cd7bfc3041babdc7139aaa00eb
aashan <goaashan@gmail.com>
2017-02-19 22:36:24 +0530

    conflict

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,31 @@ particlesJS('new-drishti-container', {
         "background_size": "cover"
     }
 });
+
+
+(function(){
+
+	var button = document.getElementById('cn-button'),
+    wrapper = document.getElementById('cn-wrapper');
+
+    //open and close menu when the button is clicked
+	var open = false;
+	button.addEventListener('click', handler, false);
+
+	function handler(){
+	  if(!open){
+	    this.innerHTML = "Close";
+	    classie.add(wrapper, 'opened-nav');
+	  }
+	  else{
+	    this.innerHTML = "Menu";
+		classie.remove(wrapper, 'opened-nav');
+	  }
+	  open = !open;
+	}
+	function closeWrapper(){
+		classie.remove(wrapper, 'opened-nav');
+	}
+
+})();
+/* navigation property*/
