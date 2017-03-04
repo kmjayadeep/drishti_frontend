a4c7f6701ac24542aba80778a88250db5510483f
Jayadeep KM <kmjayadeep@gmail.com>
2017-03-04 07:12:32 +0530

    added onepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,20 @@ $(document).ready(function() {
         setTimeout(function() {
             $('#old-drishti-container').slideUp()
             $("body").css("overflow-y", "scroll");
+            $('#onepage').onepage_scroll({
+                sectionContainer: ".section",
+                loop:false
+            })
             setTimeout(function() {
                 $('#dexter').fadeOut()
             }, 1000)
         }, 9000)
+    } else {
+        $('#onepage').onepage_scroll({
+            sectionContainer: ".section",
+            loop:false
+        })
     }
-
-
 });
 
 function showLoading() {
